4f24a49300f487061198d3c9ec9f26f1a8ba975d
msmenegol <marcelosmenegol@gmail.com>
2017-03-08 17:19:40 -0300

    Clean some commented code

--- JasonBulb.java
+++ JasonBulb.java
@@ -107,92 +107,9 @@ public class JasonBulb implements Runnable{
     return this.mailbox.remove(message);
   }
 
-/*
-    // writes and receives the full message int the socket (String)
-  public String sendReceive(String message) {
-    //System.out.println("beginning send");
-    if(this.socket!=null){
-
-      try{
-        //System.out.println(socket.getInetAddress().isReachable(1000));
-        if(!socket.getInetAddress().isReachable(timeout)){
-          //if it isn't reachable or timeout of 1s
-          System.out.println("not reachable");//do something
-        }
-      } catch(Exception e){e.printStackTrace();}
-
-      try {
-        //System.out.println("trying send");
-        // out & in
-        PrintWriter out = new PrintWriter(getSocket().getOutputStream(), true);
-        BufferedReader in = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
-        // writes str in the socket and read
-        //System.out.println(message + " sent");
-        out.println(message);
-
-        //if(this.checkEmergency()){return "notReady";}//do not proceed while an emergency is happening
-
-        String returnStr = in.readLine();
-        return returnStr;
-      } catch (IOException e) {
-          e.printStackTrace();
-      }
-    }
-    //System.out.println("socket is null");
-    return "notReady";//try again
-  }
-*/
-
     // get the socket instance
   private Socket getSocket() {
        return socket;
   }
 
-/*
-  public boolean checkEmergency(){
-    if (this.socket!=null){
-      try{
-        //System.out.println(socket.getInetAddress().isReachable(1000));
-        if(!socket.getInetAddress().isReachable(1000)){
-          //if it isn't reachable or timeout of 1s
-          System.out.println("not reachable");//do something
-          return false;
-        }
-      } catch(Exception e){e.printStackTrace();}
-
-      try{
-        BufferedReader in = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
-        char c = peek(in);
-        System.out.println(c);
-        if(c == '!'){//if it's an emergency
-          System.out.println("EMERGENCY!");
-          //BufferedReader in = new BufferedReader(new InputStreamReader(getSocket().getInputStream()));
-          String emergencyID = in.readLine();//get whole emergencyID
-          in.close();
-          cortex.emergency(emergencyID);//transmit emergency to cortex
-          return true;
-        }
-        in.close();
-        System.out.println("notEm");
-        return false;//else {//otherwise
-          //System.out.println("putting it back");
-          //peekableIn.unread(c);//put thingS back in place
-          //return false;
-        //}
-      } catch(Exception e){
-        return false;
-      }
-    } return false;
-  }
-
-  private char peek(BufferedReader buf){
-    try{
-      buf.mark(1);
-      char b = (char) buf.read();
-      buf.reset();
-      return b;
-    } catch(Exception e){e.printStackTrace();}
-    return '\0';
-  }
-*/
 }
