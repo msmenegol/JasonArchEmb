6953217880a67e15d1c33dd17a0b9f133f0233af
msmenegol <marcelosmenegol@gmail.com>
2017-03-10 00:24:47 -0300

    Remove unused code

--- EJasonArch.java
+++ EJasonArch.java
@@ -56,7 +56,6 @@ public class EJasonArch extends AgArch {
     public void act(ActionExec action) {
 
         String s = actionToString(action);
-        System.out.println("action is " + s);
 
         bulb.bulbSend(encodeAction(actionToString(action)));
 
@@ -125,13 +124,11 @@ public class EJasonArch extends AgArch {
 
 
     public void confirmAction(String actionStr){
-      System.out.println("Confirmation was called with " + actionStr);
       Iterator it = waitingConfirmList.entrySet().iterator();
       while(it.hasNext()){
         Map.Entry pair = (Map.Entry)it.next();
         if(pair.getValue().equals(actionStr)){
           //set that the execution was ok
-          System.out.println("Confirming " + pair.getValue());
           ((ActionExec)pair.getKey()).setResult(true);
           actionExecuted((ActionExec)pair.getKey());
           it.remove();
@@ -148,7 +145,6 @@ public class EJasonArch extends AgArch {
     }
 
     private String encodeAction(String message){
-      System.out.println("Encoded " + message + "   " + this.actionID);
       return this.actionID+message;
     }
 
