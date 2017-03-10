c66fa6ca3cd3596f4a475a92fa67ee0cf2a1a62a
msmenegol <marcelosmenegol@gmail.com>
2017-03-09 23:42:45 -0300

    Padronize filter

--- UAVexample/PerceptFilter.java
+++ UAVexample/PerceptFilter.java
@@ -4,12 +4,17 @@ import java.lang.*;
 public class PerceptFilter{
   //for percepts without args
   public static boolean filter(String functor){
-    return true;
+    switch (functor) {
+      //ADD FUNTOR-ONLY CASES HERE
+      default:
+        return true;
+    }
   }
 
   //for percepts with args
   public static boolean filter(String functor, String[] newTerms, List<String[]> oldTerms){
     switch (functor) {
+      //ADD OTHER PERCEPTS CASES HERE
       case "position":
 /* FILTER POSITION */
         Double[] newXYZ = new Double[newTerms.length];
